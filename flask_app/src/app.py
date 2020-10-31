6c8587054a63338ea244c73ecd5bb463e752b4cb
bigsem89 <bigsem89@gmail.com>
2020-10-31 22:36:16 +0300

    get data from html and put it to database

--- flask_app/src/app.py
+++ flask_app/src/app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-server = Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbuser:123@db/testdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@server.route("/")
-def hello():
-    return "Hello World!"
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(10), nullable=False)
+    lname = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return 'Person %r' % self.id
+
+
+db.create_all()
+
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        fname = request.form['fname']
+        lname = request.form['lname']
+
+        person = Person(fname=fname, lname=lname)
+
+        db.session.add(person)
+        db.session.commit()
+        return redirect("/")
+    else:
+        return render_template("form.html")
 
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
